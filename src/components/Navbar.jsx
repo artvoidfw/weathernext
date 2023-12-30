7e95889e93fc52719f24768424dd33aecfff9f83
artvoidfw <artvoidcodew@gmail.com>
2023-12-30 11:53:45 +0400

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <>
         <div className="navbar">
-        <i class="fa-brands fa-github"></i>
+        <Link href="https://github.com/artvoidfw/"><i class="fa-brands fa-github"></i></Link>
         </div>
         </>
     )
